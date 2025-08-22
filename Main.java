ff843530bcdc0156c387b2b86546b6fa7b696c46
RoberthKennedy <robertharaujo81@gmail.com>
2025-08-22 15:54:31 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SistemaDeMensagens sistema = new SistemaDeMensagens();
+        sistema.processarMensagem();
+    }
+}
